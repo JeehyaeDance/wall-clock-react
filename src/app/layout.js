7d7827d12593f07b5897092964641b5db574b1d4
JeehyaeDance <jeehyae91@gmail.com>
2024-06-27 11:36:01 +0900

    edit readme and meta data for page

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Wall Clock React",
-  description: "coding test for Adriel",
+  description: "Analog wall clock",
 };
 
 export default function RootLayout({ children }) {
